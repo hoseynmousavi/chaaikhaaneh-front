630c0a450879ae735db769119e872ee8cd6e6b0e
Seyed Hoseyn <hoseyn.mousavi78@gmail.com>
2025-12-22 14:05:39 +0330

    init

--- src/views/pages/HomePage.tsx
+++ src/views/pages/HomePage.tsx
@@ -1,7 +1,12 @@
 import Head from "views/components/router/Head"
 
 function HomePage() {
-	return <Head title={"services"} />
+	return (
+		<>
+			<Head title={"services"} />
+			<div>Chai Khane</div>
+		</>
+	)
 }
 
 export default HomePage
